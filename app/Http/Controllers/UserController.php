362354f0d5a6055aac7be8fe18c3fca71134a7e1
Isuru Indika <isuruindikauom@gmail.com>
2023-11-17 21:03:44 +0000

    Task : Add rolewise user validation with test cases  # On branch user-crud/isuru Changes to be committed: M	app/Http/Controllers/UserController.php M	tests/Feature/UserManagementTest.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,12 @@ class UserController extends Controller
 {
     public function store(UserCreateRequest $request)
     {
+        if (in_array(config('roles.' . $request->role_id), ['Developer', 'Admin'])) {
+            $request->validate([
+                'email' => ' required',
+                'password' => 'required',
+            ]);
+        }
         $user = User::create($request->all());
         return response()->json($user);
     }
--- tests/Feature/UserManagementTest.php
+++ tests/Feature/UserManagementTest.php
@@ -37,6 +37,55 @@ class UserManagementTest extends TestCase
         $this->assertNotEquals(array_merge($this->data(), ['password' => 'password123'])['password'], $user->password);
         $this->assertTrue(Hash::check(array_merge($this->data(), ['password' => 'password123'])['password'], $user->password));
     }
+
+    /**
+     * Driver role id is 3
+     * with only name and mobile number driver should be added to the system
+     */
+    public function test_a_driver_can_be_added_to_the_system(): void
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('api/users', array_merge($this->data(), ['role_id' => 3, 'address' => '', 'email' => '', 'password' => '']));
+        $this->assertCount(1, User::all());
+        foreach (array_merge($this->data(), ['role_id' => 3, 'address' => '', 'email' => '', 'password' => '']) as $key => $value) {
+            if ($key != "password") {
+                $response->assertSee($value);
+            }
+        }
+    }
+    /**
+     * Developer and admin roles 1,2
+     * with only name, mobile number, email and password developer and admin should be added to the system
+     */
+    public function test_a_developer_and_admin_can_be_added_to_the_system(): void
+    {
+        $this->withoutExceptionHandling();
+        $response = $this->post('api/users', array_merge($this->data(), ['role_id' => 1, 'address' => '', 'email' => 'dev@taxi.com', 'password' => bcrypt('password123')]));
+        $this->assertCount(1, User::all());
+        foreach (array_merge($this->data(), ['role_id' => 1, 'address' => '', 'email' => 'dev@taxi.com', 'password' => '']) as $key => $value) {
+            if ($key != "password") {
+                $response->assertSee($value);
+            }
+        }
+        $response = $this->post('api/users', array_merge($this->data(), ['role_id' => 2, 'mobile_number' => '0717196599', 'address' => '', 'email' => 'adminz@taxi.com', 'password' => bcrypt('password123')]));
+        $this->assertCount(2, User::all());
+        foreach (array_merge($this->data(), ['role_id' => 2, 'mobile_number' => '0717196599', 'address' => '', 'email' => 'adminz@taxi.com', 'password' => '']) as $key => $value) {
+            if ($key != "password") {
+                $response->assertSee($value);
+            }
+        }
+    }
+
+    public function test_a_developer_and_admin_cannot_be_added_to_the_system_without_email(): void
+    {
+        $response = $this->post('api/users', array_merge($this->data(), ['role_id' => 1, 'address' => '', 'email' => '', 'password' => bcrypt('password123')]));
+        $response->assertSessionHasErrors('email');
+
+        $response = $this->post('api/users', array_merge($this->data(), ['role_id' => 2, 'mobile_number' => '0717196599', 'address' => '', 'email' => 'abc@taxi.com', 'password' => '']));
+        $response->assertSessionHasErrors('password');
+
+    }
+
     public function data()
     {
         return [
