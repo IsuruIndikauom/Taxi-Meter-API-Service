cacb2c98acd8ccfd416c1140f3c04d8360390419
Isuru Indika <isuruindikauom@gmail.com>
2023-11-26 19:23:03 +0000

    Task : Handle not found routes  # On branch crud_for_admin_for_tarrif/isuru Changes to be committed: M	app/Exceptions/Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Handler extends ExceptionHandler
@@ -29,11 +30,30 @@ class Handler extends ExceptionHandler
         });
     }
 
+    // public function render($request, Throwable $exception)
+    // {
+    //     if ($exception instanceof ModelNotFoundException && $request->expectsJson()) {
+    //         return response()->json(['error' => 'Cannot find this id'], 404);
+    //     }
+    //     return parent::render($request, $exception);
+    // }
+
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof NotFoundHttpException) {
+            // Handle the 'NotFoundHttpException' condition here
+            if ($request->expectsJson()) {
+                return response()->json(['error' => 'Route not found'], 404);
+            }
+            // For other cases, you might redirect or show a view
+            // return redirect('/not-found');
+        }
+
         if ($exception instanceof ModelNotFoundException && $request->expectsJson()) {
+            // Handle the 'ModelNotFoundException' condition here
             return response()->json(['error' => 'Cannot find this id'], 404);
         }
+
         return parent::render($request, $exception);
     }
 }
